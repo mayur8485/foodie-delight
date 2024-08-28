84b8aa28d7b0b49d051ec7d66d39e0e4a6aeac8e
Mayuresh <mayuresh.takawane@dista.ai>
2024-08-28 23:38:18 +0530

    table component

---
+++ foodie-delight/src/app/table/table.component.ts
@@ -0,0 +1,54 @@
+import { Component, Input } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent {
+  @Input() data: any;
+
+  dataChunks: any;
+
+  page: number = 0;
+  noOfPages: any = [];
+  recordsPerPage: number = 5;
+  startIndex: number = 0;
+
+  constructor(private router: Router, private dataService: DataService) {
+    this.dataService.dataSubject.subscribe((data) => {
+      this.data = data;
+      if (this.data) {
+        this.noOfPages = Math.ceil(this.data.length / this.recordsPerPage) * 10;
+        this.addPagination(1);
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.data = this.dataService.getRestaurants();
+    if (this.data) {
+      this.noOfPages = Math.ceil(this.data.length / this.recordsPerPage) * 10;
+      this.addPagination(1);
+    }
+  }
+
+  addPagination(page: number) {
+    const startIndex = ((page - 1) * this.recordsPerPage);
+    const endIndex = startIndex + this.recordsPerPage;
+    this.startIndex = startIndex;
+    this.dataChunks = this.data.slice(startIndex, endIndex)
+  }
+
+  onEdit(id: any, index: any) {
+    this.router.navigate(['./adminForm', id])
+
+  }
+
+  onDelete(id: any, index: any) {
+    this.dataService.deleteRestaurant(id, index);
+  }
+
+}
